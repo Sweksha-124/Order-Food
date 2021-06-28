641f62100a549cff126b31a1ffc5f61f2cd92dcd
Nikhil.Rathi <nikhil.rathi@brandmuscle.com>
2021-06-28 10:19:13 +0530

    Impl. OrderFood

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OrderFood';
+  menuList:any[]=[
+    {id:1,name:'Burger',price:10},
+    {id:2,name:'Momos',price:20},
+    {id:3,name:'Dosa',price:30},
+    {id:4,name:'Noodles',price:40},
+    {id:5,name:'Soup',price:50},
+  ];
+  orderList:any[]=[];
+  getOrder:boolean=false;
+  getBill:boolean=false;
+  tipPercentage:number=10;
+
+  OrderList(id:number){
+    if(this.orderList.length<1 || this.orderList.filter(e => e.id === id).length <1){
+      let item=this.menuList.find(x=> x.id==id);
+      item['quantity']=1;
+      this.orderList.push(item);
+    }
+    else{
+      let index=this.orderList.findIndex(x=> x.id==id);
+      this.orderList[index]['quantity']=parseInt(this.orderList[index]['quantity'])+1;
+    }
+  }
+
+  BillAmount():number{
+    let amount:number=0;
+    if(this.orderList.length>0){
+      this.orderList.forEach(food=>{
+        amount += food.price * food.quantity;
+      });
+    }
+    amount= this.ApplyTip(amount);
+    return amount;
+  }
+ApplyTip(amount:number){
+if(this.tipPercentage>0){
+  amount += ((amount*this.tipPercentage)/100);
+}
+return amount;
+}
+  CancelFoodOrder(id:number){
+    if(this.orderList.length>0){
+      let index=this.orderList.findIndex(x=> x.id==id);
+      if(index>-1){
+        if(this.orderList[index]['quantity']>1){
+          this.orderList[index]['quantity']=parseInt(this.orderList[index]['quantity'])-1;          
+        }
+        else{
+          this.orderList.splice(index, 1);
+        }
+      }
+    }
+  }
 }
